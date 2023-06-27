8e36a5c34ac233e1a9c440b34897a0ef54deeff6
Sayyedbehzad Hashemian <shashe43@uwo.ca>
2023-06-27 11:40:11 -0400

    updating Clients section

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -4,9 +4,9 @@ import { clients } from '../constants'
 const Clients = () => {
   return (
     <section>
-      <div className='flex flex-row justify-around items-center mx-10 px-10 my-10'>
+      <div className='flex md:flex-row flex-col justify-around items-center mx-10 px-10 my-10'>
         {clients.map((cli,index) => (
-          <img src={cli.logo} alt="logo" key={cli.id} className='w-[192px]'/>
+          <img src={cli.logo} alt="logo" key={cli.id} className='w-[192px] py-8'/>
         ))}
       </div>
     </section>

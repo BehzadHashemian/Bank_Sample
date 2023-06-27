edd3a0e3adbc9ef7807a5b42be9b828d4f93b6e3
Sayyedbehzad Hashemian <shashe43@uwo.ca>
2023-06-26 23:59:37 -0400

    creating Testimonials section

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
-
+import styles from '../style'
+import { feedback } from '../constants'
+import { quotes } from '../assets'
 const Testimonials = () => {
   return (
-    <div>Testimonials</div>
+    <section >
+      <div className='flex flex-col py-10'>
+        <div className='flex md:flex-row flex-col flex-1 justify-around items-center'>
+          <h1 className={`${styles.heading2} md:w-[470px] md:-ml-16 my-10`}>What people are saying about us</h1>
+          <p className={`${styles.paragraph} md:w-[450px] md:mr-16 my-10`}>Everything you need to accept card payments and grow your business anywhere on the planet.</p>
+        </div>
+        <div className='flex md:flex-row flex-col justify-between items-center'>
+          {feedback.map((feed,index) =>(
+            <div className='flex flex-col mx-4 w-[370px] p-10 feature-card rounded-3xl' key={index}>
+              <img src={quotes} alt="quots" className='w-[42px] mb-10' />
+              <div>
+                <p className={`${styles.paragraph} `}>Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver.</p>
+              </div>
+              <div className='flex flex-row mt-4'>
+                <img src={feed.img} alt="profile" className='w-[48] h-[48px]'/>
+                <div className='flex flex-col'>
+                  <span className='text-white font-poppins text-[20px] pl-6'>{feed.name}</span>
+                  <span className={`pl-6 ${styles.paragraph}`}>{feed.title}</span>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
   )
 }
 

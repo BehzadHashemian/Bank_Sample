93975eb51f3f2e08f4d106df9a41067e1a5ca642
Sayyedbehzad Hashemian <shashe43@uwo.ca>
2023-06-26 23:04:20 -0400

    change round redius

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = () => {
   return (
-    <button type='button' className='py-4 px-6 bg-blue-gradient font-poppins rounded-md text-primary outline-none'>
+    <button type='button' className='py-4 px-6 bg-blue-gradient font-poppins rounded-xl text-primary outline-none'>
       Get Started
     </button>
   )

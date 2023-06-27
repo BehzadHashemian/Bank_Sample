3c5f339be74b26e5da0c304039495a355f2dbe7c
Sayyedbehzad Hashemian <shashe43@uwo.ca>
2023-06-27 15:01:54 -0400

    fixing margin in CTA

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -7,7 +7,7 @@ const CTA = () => {
   return (
     <section className='flex flex-1 justify-center my-6'>
       <div className='flex md:flex-row flex-col justify-around items-center bg-black-gradient-2 z[20] rounded-2xl md:w-[1170px] md:h-[293] py-10 my-10'>
-        <div className='mb-4 px-10'>
+        <div className='mb-4'>
         <h1 className={`${styles.heading2} md:py-4 `}>Let’s try our service now!</h1>
         <p className={`${styles.paragraph} py-4  md:w-[500px] `}>Everything you need to accept card payments and grow your business anywhere on the planet.</p>
         </div>

80af062269df6d452429c92be6335d8f82c98d42
Sayyedbehzad Hashemian <shashe43@uwo.ca>
2023-06-27 15:15:18 -0400

    fixing text margin in Testimonials

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -7,14 +7,14 @@ const Testimonials = () => {
     <section >
       <div className='flex flex-col py-10 relative'>
         <div className='flex md:flex-row flex-col flex-1 justify-around items-center'>
-          <h1 className={`${styles.heading2} md:w-[470px] md:-ml-16 my-10`}>What people are saying about us</h1>
+          <h1 className={`${styles.heading2} md:w-[470px]  my-10`}>What people are saying about us</h1>
           <p className={`${styles.paragraph} md:w-[450px] md:mr-16 my-10`}>Everything you need to accept card payments and grow your business anywhere on the planet.</p>
         </div>
         <div className='flex md:flex-row flex-col justify-center relative'>
           {feedback.map((feed,index) =>(
-            <div className='flex justify-around flex-col mx-4 w-[370px] p-10 feature-card rounded-3xl z[1]' key={index}>
+            <div className='flex flex-col justify-between mx-4 w-[370px] p-10 feature-card rounded-3xl z[1]' key={index}>
               <img src={quotes} alt="quots" className='w-[42px] mb-10' />
-              <div >
+              <div>
                 <p className={`${styles.paragraph} `}>{feed.content}</p>
               </div>
               <div className='flex flex-row mt-4 '>
